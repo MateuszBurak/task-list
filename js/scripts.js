b36f7c0a29c60c7c40e4d554ba1814d37e8ff605
Mateusz Burak <mateuszburakemail@gmail.com>
2024-02-05 13:51:50 +0100

    added a list, basic js functionality and list styling

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+{
+    const tasks = [
+        {
+            content: "zrobić zadanie domowe",
+            done: false,
+        },
+        {
+            content: "iść już spać Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, voluptate? Omnis, impedit! Officiis amet id delectus nisi animi velit in quidem corrupti reiciendis, ad autem eum est dolores, consectetur voluptate?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi assumenda earum doloribus culpa illum voluptate cupiditate, fuga, voluptatum aspernatur rerum corporis optio amet accusamus? Placeat laudantium explicabo ipsa veniam distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quidem delectus repudiandae aliquid, officia corrupti necessitatibus, fugit similique illum molestiae vero hic quis accusamus sapiente unde qui tempore cumque saepe. Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nobis laudantium eaque tenetur? Nesciunt corporis eum commodi quisquam recusandae, cupiditate, esse, consequatur odio veritatis quos iste libero ullam voluptatibus consectetur.",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+        const list = document.querySelector(".js-list");
+
+        for (const task of tasks) {
+            htmlString += `
+            <li class="taskList__listItem">
+                <button class="taskList__buttonDone">✔</button>
+                    <div ${task.done ? "style=\"text-decoration: line-through;\"" : ""} class=\"taskList__listItemContent"\>${task.content}</div>
+                <button class="taskList__buttonDeleteTask">🗑</button>
+            </li>
+            `;
+        };
+
+        list.classList.toggle("taskList__list");
+
+        document.querySelector(".js-list").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
